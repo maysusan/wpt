2d165249193286bc998e4211fa28d8cf5fb5c1f9
Philip Jägenstedt <philip@foolip.org>
2019-10-14 15:55:23 +0200

    Decrease capture framerate

--- tools/ci/taskcluster-x11grab.sh
+++ tools/ci/taskcluster-x11grab.sh
@@ -11,7 +11,7 @@ get_filename () {
 # capture 10 seconds of video every 10 minutes for at most 2 hours
 seq 12 | while read counter; do
   FILENAME=`get_filename mp4`
-  ffmpeg -y -video_size "${SCREEN_WIDTH}x${SCREEN_HEIGHT}" -framerate 60 \
+  ffmpeg -y -video_size "${SCREEN_WIDTH}x${SCREEN_HEIGHT}" -framerate 30 \
     -f x11grab -i "$DISPLAY" -t 10 "$FILENAME"
   sleep 590
 done
